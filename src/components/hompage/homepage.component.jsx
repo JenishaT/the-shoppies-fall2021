8368306355dcbf06cfdf94b888bbd891017cf5a4
unknown <jenishathomas00@gmail.com>
2021-05-07 00:01:10 -0400

    cleaned up homepage componenet files

--- src/components/hompage/homepage.component.jsx
+++ src/components/hompage/homepage.component.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import "./homepage.styles.scss";
-
 import Button from '@material-ui/core/Button';
 
+import "./homepage.styles.scss";
 
 class Homepage extends React.Component {
-
-    componentDidMount() {
-        document.documentElement.style.setProperty('--left-curtain-pos', -100);
-        document.documentElement.style.setProperty('--right-curtain-pos', 100);
-    }
-
+    
     render() {
         return (
             <div className="curtain">
